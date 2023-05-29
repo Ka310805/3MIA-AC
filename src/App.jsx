ccf1501aee629bffb04aa066560be4463cb47a59
Ka310805 <84384342+Ka310805@users.noreply.github.com>
2023-05-29 13:43:05 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Carrosel from './components/Carrosel'
+import NavProjeto from './components/NavProjeto'
+import Cabeçalho from './components/Cabeçalho'
+import Rodape from './components/Rodape'
+import Cards from './components/Cards'
+
+function App() {
+  return (
+    <div>
+     <Cabeçalho/>
+     <NavProjeto />
+     <Carrosel/>
+     <Cards/>
+     <Rodape/>
+    </div>
+  )
+} 
+ export default App
